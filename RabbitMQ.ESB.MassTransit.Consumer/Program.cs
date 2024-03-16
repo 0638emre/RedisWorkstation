496824bb3e3ead65082cb4ba31c6be445752da46
emrec <emrecoskun06@hotmail.com>
2024-03-16 16:40:07 +0300

    massrt

---
+++ RabbitMQ.ESB.MassTransit.Consumer/Program.cs
@@ -0,0 +1,20 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Consumer.Cunsumers;
+
+string rabbitMqUri = "amqps://uulwhncp:M9EHb7NXfucL4JiXSzOl_j2TuBnMTA_6@turkey.rmq.cloudamqp.com/uulwhncp";
+
+string queueName =  "exsmple-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+   factory.Host(rabbitMqUri);
+   
+   factory.ReceiveEndpoint(queueName, endpoint =>
+   {
+      endpoint.Consumer<ExampleMessageConsumer>();
+   });
+});
+
+await bus.StartAsync();
+
+Console.Read();
---
+++ RabbitMQ.ESB.MassTransit.Publisher/Program.cs
@@ -0,0 +1,23 @@
+﻿using MassTransit;
+using RabbitMQ.ESB.MassTransit.Shared.Messages;
+
+string rabbitMqUri = "amqps://uulwhncp:M9EHb7NXfucL4JiXSzOl_j2TuBnMTA_6@turkey.rmq.cloudamqp.com/uulwhncp";
+
+string queueName = "example-queue";
+
+IBusControl bus = Bus.Factory.CreateUsingRabbitMq(factory =>
+{
+    factory.Host(rabbitMqUri);
+});
+
+ISendEndpoint sendEndpoint = await bus.GetSendEndpoint(new($"{rabbitMqUri}/{queueName}"));
+
+Console.WriteLine("Gönderilecek mesaj : ");
+
+string message = Console.ReadLine();
+await sendEndpoint.Send<IMessage>(new ExampleMessage()
+{
+    Text = message
+});
+
+Console.Read();
